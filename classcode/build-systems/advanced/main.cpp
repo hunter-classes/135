a29da38f297736c8efe5256a279251194f0508b5
Mike Zamansky <zamansky@gmail.com>
2021-02-11 08:57:25 -0500

    finished this version

--- classcode/build-systems/advanced/main.cpp
+++ classcode/build-systems/advanced/main.cpp
@@ -2,11 +2,14 @@
 
 #include "comparisons.h"
 #include "adders.h"
+
+
+
 int main(int argc, char *argv[])
 {
   int a = 50;
   int b = 10;
-
+  int d;
   int c = smaller(a,b);
 
   std::cout << c << std::endl;
