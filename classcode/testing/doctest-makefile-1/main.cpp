cfbcc1bd464bb3b626d384b2606eaf5cf73c7298
Mike Zamansky <zamansky@gmail.com>
2021-02-22 09:11:01 -0500

    added Makefile example

---
+++ classcode/testing/doctest-makefile-1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#define DOCTEST_CONFIG_IMPLEMENT
+
+#include "doctest.h"
+#include "fact.h"
+
+int main() 
+{
+  std::cout << "calling fact(5) : " << fact(5) << "\n";
+  return 0;
+}
