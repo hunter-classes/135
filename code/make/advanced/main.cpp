835936e107586853c61f1d9de744b91de535e689
Mike Zamansky <zamansky@gmail.com>
2021-09-13 09:22:54 -0400

    added advanced sample

---
+++ code/make/advanced/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "mymath.h"
+
+
+int main()
+{
+  int x;
+  x = add_two(20,30);
+  std::cout << "20+30="<< x << "\n";
+  
+  return 0;
+}
---
+++ code/make/advanced/mymath.cpp
@@ -0,0 +1,7 @@
+#include "mymath.h"
+
+int add_two(int a, int b){
+
+  return a+b;
+}
+
