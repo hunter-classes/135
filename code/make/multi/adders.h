9fca72add2bd283dd57f9cbde0b36fd14412da80
Mike Zamansky <zamansky@gmail.com>
2022-02-07 09:23:19 -0500

    added base for multi makefile

---
+++ code/make/multi/adders.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int add2(int a, int b);
+int add3(int a, int b, int c);
---
+++ code/make/multi/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "adders.h"
+#include "utils.h"
+
+int main()
+{
+  std::cout << "Hello World!\n";
+  return 0;
+}
