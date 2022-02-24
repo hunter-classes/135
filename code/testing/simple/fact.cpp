31860eb379b416169d29f3fbcbc9b5f911d23733
Mike Zamansky <zamansky@gmail.com>
2022-02-24 09:03:02 -0500

    added simple test example

---
+++ code/testing/simple/fact.cpp
@@ -0,0 +1,27 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <iostream>
+#include "doctest.h"
+
+int fact(int n){
+  if (n <= 1){
+    return 1;
+  } else {
+    return n * fact(n-1);
+  }
+  
+}
+
+TEST_CASE("Factorial Base Cases"){
+  CHECK(fact(0) == 1);
+  CHECK(fact(1) == 1);
+}
+
+TEST_CASE("Factorial of small numbers"){
+  CHECK(fact(2) == 2);
+  CHECK(fact(3) == 6);
+  CHECK(fact(4) == 24);
+}
+
+TEST_CASE("Factorial of big numbers"){
+  CHECK(fact(5) == 120);
+}
