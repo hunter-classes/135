47fcaae10780a1cb70adde545b0732616e1840ed
Mike Zamansky <zamansky@gmail.com>
2022-04-07 08:35:09 -0400

    added git scripts

--- code/testing/doctest-robust-example/fact-tests.cpp
+++ code/testing/doctest-robust-example/fact-tests.cpp
@@ -10,6 +10,10 @@ TEST_CASE("Factorial of small numbers"){
   CHECK(fact(2) == 2);
   CHECK(fact(3) == 6);
   CHECK(fact(4) == 24);
+  
+  for (int i = 3; i < 20; i++){
+      CHECK(fact(i) == fact(i));
+    }
 }
 
 TEST_CASE("Factorial of big numbers"){
