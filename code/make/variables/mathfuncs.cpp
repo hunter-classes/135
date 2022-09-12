da2fde2c1b13b6449f3c3f0f37f21e29e55098c7
Mike Zamansky <zamansky@gmail.com>
2022-09-12 10:31:46 -0400

    Git with variables

---
+++ code/make/variables/mathfuncs.cpp
@@ -0,0 +1,10 @@
+#include "mathfuncs.h"
+
+int add_two(int a, int b){
+  int c = a+b;
+  return c;
+}
+
+int add_three(int a, int b, int c){
+  return a+b+c;
+}
---
+++ code/make/variables/mathfuncs.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int add_two(int,int);
+int add_three(int,int,int);
+
+
