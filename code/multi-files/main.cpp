fd7ee1553f07560c2f1b5cc35c01e84135f37ab0
Mike Zamansky <zamansky@gmail.com>
2023-01-30 10:15:50 -0500

    added add2 in main

--- code/multi-files/main.cpp
+++ code/multi-files/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
+int add2(int a, int b) {
+  int c;
+  c = a + b;
+  return c;
+}
+
 
 int main()
 {
   std::cout << "Hello world!\n";
-  
+  int x = add2(5,8);
+
+  std::cout << "5 + 8 = " << x << "\n";
   return 0;
 }
